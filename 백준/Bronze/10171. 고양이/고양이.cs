bf5e738a37699ac152f1ba37b47cd1f057994483
전동현 <70741953+jundonghyun@users.noreply.github.com>
2022-08-09 13:01:16 +0900

    [Bronze V] Title: 고양이, Time: 60 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+
+namespace NotePad
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+           Console.WriteLine("\\    /\\");
+           Console.WriteLine(" )  ( ')");
+           Console.WriteLine("(  /  )");
+           Console.WriteLine(" \\(__)|");
+        }
+    }
+}
