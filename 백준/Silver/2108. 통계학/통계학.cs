f698532c98eb32d1a00128461664c86ed9d4def0
전동현 <70741953+jundonghyun@users.noreply.github.com>
2022-08-10 13:00:43 +0900

    [Silver III] Title: 통계학, Time: 248 ms, Memory: 21948 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.cs
+++ 백준/Silver/2108. 통계학/통계학.cs
@@ -44,7 +44,7 @@ namespace NotePad
                 if (min > arr[i])
                 {
                     min = arr[i];
-                }
+                } //범위를 구하기 위해서 최대값과 최소값을 판별하는 곳
                 
                 int count = 1;
 
@@ -52,19 +52,19 @@ namespace NotePad
                 {
                     avg += arr[i];
                     count = dict[arr[i]];
-                    dict[arr[i]] = ++count;
+                    dict[arr[i]] = ++count; //키가 있다면 중복되는 값이 있다는 것이므로 딕셔너리 안에 이미 값이 있으므로 중복값의 count를 늘려줌
+                    
                     if (maxCount <= count)
                     {
-                        maxCount = count;
-                        maxValue = arr[i]; //최빈값
+                        maxCount = count; //최대 빈도수를 갱신
                     }
                     continue;
                 }
-                avg += arr[i];
+                avg += arr[i]; //키가 딕셔너리 안에 없을경우 새로 넣어줌
                 dict.Add(arr[i], count);
             }
 
-            foreach (var i in dict)
+            foreach (var i in dict) // 딕셔너리의 값을 value(count)만 빼서 리스트에 넣고 정렬하면 오름차순으로 정렬됨
             {
                 if (i.Value == maxCount)
                 {
@@ -73,15 +73,13 @@ namespace NotePad
             }
             list.Sort();
             
-            // dict = dict.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
-            // dict = dict.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
             Array.Sort(arr);
 
-            double temp = avg / idx;
+            double temp = avg / idx; // 산술평균을 구하는곳
             
-            if (maxCount == 1) //최빈값이 없는것 두번째로 작은값 출력해야함
+            if (maxCount == 1) //겹치는게 없음
             {
-                if (idx == 1)
+                if (idx == 1) //겹치는게 없고 입력된 값자체가 한개일때
                 {
                     highest = arr[0];
                     avg = arr[0];
@@ -95,42 +93,42 @@ namespace NotePad
                     Console.WriteLine(range);
                     return;
                 }
-                else //두번째로 작은 값 출력
+                else //겹치는건 없지만 입력된 값이 여러개일때 ex)1, 2, 3, 4
                 {
-                    highest = list[1];
+                    highest = list[1]; //리스트정렬을 통해 오름차순으로 정렬되어 있으므로 문제에서준 조건인 최빈값이 여러개라면 두번째로 작은 값이므로 리스트의 두번째 값을 가져옴
                 }
-                if (temp < 0)
+                if (temp < 0) //평균값이 음수라면
                 {
-                    if (temp.ToString().Contains('.'))
+                    if (temp.ToString().Contains('.')) //평균값이 소수점이라면
                     {
-                        string[] t1 = temp.ToString().Split('.');
-                        if (5 > int.Parse(t1[1][0].ToString())) // .5 > .3
+                        string[] t1 = temp.ToString().Split('.'); //소수점 앞뒤로 나눔 (소수점 뒷자리 첫번째 자리만 사용할 것)
+                        if (5 > int.Parse(t1[1][0].ToString())) // .5 > .3 ex) -4.123에서 .으로 split한것중 첫번째값 1이 5보다 작으면 버림해야함.
                         {
-                            avg = (int)Math.Ceiling(temp);
+                            avg = (int)Math.Ceiling(temp); //소수점 아래 올림 ex)-4.31일때 값이 -4가 나와야함
                         }
                         else
                         {
-                            avg = (int)Math.Floor(temp);
+                            avg = (int)Math.Floor(temp); //5보다 크다면 버림 ex)-4.59일때 값이 -5가 나와야함.
                         }
                     }
-                    else
+                    else //평균값이 소수점이 아니라면 그 자체가 평균이됨
                     {
                         avg = temp;
                     }
                 
                 }
-                else
+                else //평균값이 음수가 아니라면
                 {
                     if (temp.ToString().Contains('.'))
                     {
                         string[] t1 = temp.ToString().Split('.');
-                        if (5 > int.Parse(t1[1][0].ToString())) // .5 > .3
+                        if (5 > int.Parse(t1[1][0].ToString())) // .5 > .3 
                         {
-                            avg = (int)Math.Floor(temp);
+                            avg = (int)Math.Floor(temp); //ex) 1.4라면 1로 버림해야함
                         }
                         else
                         {
-                            avg = (int)Math.Ceiling(temp);
+                            avg = (int)Math.Ceiling(temp); //ex) 1.6이면 2로 올림해야함
                         }
                     }
                     else
