0fcc6cd81e53095c81afa09a2bbffa9f8e8d2689
전동현 <70741953+jundonghyun@users.noreply.github.com>
2022-08-16 14:55:31 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 64 ms, Memory: 5920 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Text;
+
+namespace NotePad
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            var writer = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int[,] arr = new int[15,15];
+
+            for (int i = 0; i < 15; i++)//층
+            {
+                for (int j = 1; j < 15; j++)//호
+                {
+                    if (i == 0)
+                    {
+                        arr[i, j] = j;
+                    }
+                    else
+                    {
+                        arr[i, j] = arr[i - 1, j] + arr[i,j-1];
+                    }
+                }
+            }
+
+            int n = int.Parse(reader.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                writer.WriteLine(arr[int.Parse(reader.ReadLine()), int.Parse(reader.ReadLine())]);
+            }
+
+            writer.Close();
+            reader.Close();
+        }
+    }
+}
