f5002c5ac63709ad5b8a735911153f8610a2a429
전동현 <70741953+jundonghyun@users.noreply.github.com>
2022-09-19 09:55:14 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.16 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cs
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cs
@@ -3,19 +3,16 @@ using System;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        int min = Int32.MaxValue;
-
-        for(int i = 1; i < n; i++)
+        
+        for (int i = 1; i < n; i++)
         {
             if (n % i == 1)
             {
-                if (min > i)
-                {
-                    min = i;
-                }
+                answer = i;
+                break;
             }
         }
         
-        return min;
+        return answer;
     }
 }
