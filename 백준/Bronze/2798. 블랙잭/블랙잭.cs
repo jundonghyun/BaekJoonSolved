4e52e6a49f9c7dac14176325513f290e4f478324
전동현 <70741953+jundonghyun@users.noreply.github.com>
2022-11-23 09:04:45 +0900

    [Bronze II] Title: 블랙잭, Time: 64 ms, Memory: 5708 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cs
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cs
@@ -1,55 +1,38 @@
 using System;
+using System.Linq;
+using System.Reflection.Emit;
 
-namespace NotePad
+namespace NotePad.BaekJoon
 {
-    class Program
+    public class notepad
     {
-        /*양의 정수가 들어있는 카드 값(1~100,000)
-        바닥에 앞면의 n장(3~100개)의 카드 중 3장을 고른다.
-        숫자 m(10~300,000)을 넘지 않으면서 m과 최대한 가깝게 만들어야한다.*/
-
-
         public static void Main(string[] args)
         {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-            /*입력
-            5 21
-            5 6 7 8 9 */
+            int target = arr[1];
 
-            /*출력
-            첫째 줄에 M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 출력한다.*/
-            String[] input = Console.ReadLine().Split();
-            int n = int.Parse(input[0]);
-            int m = int.Parse(input[1]);
+            int n = arr[0];
 
-            int max = 0;
-            int dif = Int32.MaxValue;
-            int result = 0;
-            
-            int[] arr = new int[n];
-            String[] input2 = Console.ReadLine().Split();
-            
-            
-            for (int i = 0; i < input2.Length; i++)
+            int result = int.MinValue;
+
+            arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < arr.Length-2; i++)
             {
-                arr[i] = int.Parse(input2[i]);
-            }
-
-            for (int i = 0; i < arr.Length; i++)
-            {
-                for (int j = i+1; j < arr.Length; j++)
+                for (int j = i + 1; j < arr.Length - 1; j++)
                 {
-                    for (int k = j+1; k < arr.Length; k++)
+                    for (int k = j + 1; k < arr.Length; k++)
                     {
-                        max = arr[i] + arr[j] + arr[k];
-                        if (max <= m)
+                        if (arr[i] + arr[j] + arr[k] > target)
                         {
-                            int temp = m - max;
-
-                            if (dif > temp)
+                            continue;
+                        }
+                        else
+                        {
+                            if (result < arr[i] + arr[j] + arr[k])
                             {
-                                dif = temp;
-                                result = max;
+                                result = arr[i] + arr[j] + arr[k];
                             }
                         }
                     }
@@ -60,4 +43,3 @@ namespace NotePad
         }
     }
 }
-
